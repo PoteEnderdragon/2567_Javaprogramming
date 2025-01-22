c457f4f3df641871f3a0e028e8e719afa6fa1bae
Chitsanupong <Chitsanupong.puegpuang@gmail.com>
2025-01-22 08:44:13 +0700

    Lab6_Homework

---
+++ LabSheet06/src/TestIncomeTaxCalculator.java
@@ -0,0 +1,14 @@
+
+public class TestIncomeTaxCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		IncomeTaxCalculator taxCalculator = new IncomeTaxCalculator();
+		 
+        taxCalculator.setIncome(450000);
+        taxCalculator.displayTaxCalculator();
+
+	}
+}
+
+
