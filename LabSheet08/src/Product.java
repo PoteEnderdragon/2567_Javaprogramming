9f647af3c8d43b4ff78d5fc078d89ecedc222ce8
Chitsanupong <Chitsanupong.puegpuang@gmail.com>
2025-01-29 11:26:09 +0700

    Labsheet08_Practice(2)

--- LabSheet08/src/Product.java
+++ LabSheet08/src/Product.java
@@ -1,4 +1,32 @@
 
 public class Product {
+	private String productId;
+	private int productUnit;
+	private double productPrice;
+	
+	public void setId(String pid) {
+		productId = pid;
+	}
+	public String getId() {
+		return this.productId;
+	}
+	
+	public void setUnit(int proUnit) {
+		productUnit = proUnit;
+		
+	}
+	public int getUnit() {
+		return productUnit;
+				
+	}
+	public void setPrice(double proPrice) {
+		productPrice = proPrice;
+	}
+	public double getPrice() {
+		return productPrice;
+	}
+	public double calculate() {
+		return productUnit * productPrice;
+	}
 
 }
--- LabSheet08/src/StockProduct.java
+++ LabSheet08/src/StockProduct.java
@@ -1,8 +1,36 @@
-
+import java.util.*;
 public class StockProduct {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		Product[] productList = new Product[4];
+		
+		for (int i =0;i<productList.length;i++) 
+		{
+			productList[i] = new Product();
+			System.out.print("Input product id : ");
+			String id = scan.nextLine();
+			productList[i].setId(id);
+			System.out.print("Input product Unit : ");
+			int unit = scan.nextInt();
+			scan.nextLine();
+			productList[i].setUnit(unit);
+			System.out.print("Input product Price : ");
+			double price = scan.nextDouble();
+			scan.nextLine();
+			productList[i].setPrice(price);
+			System.out.println();
+		}
+		System.out.println("-----------------------------------------");
+		double totalPrice = 0;
+		for(Product product:productList) {
+			System.out.println("Product ID : "+product.getId()+"Total price = "+product.calculate()+" baht.");
+			totalPrice += product.calculate();
+		}
+		System.out.println("-----------------------------------------");
+		System.out.println("Total price of all products is "+ totalPrice + " baht.");
+		scan.close();
 
 	}
 
