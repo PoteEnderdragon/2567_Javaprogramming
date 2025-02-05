0ca1857107c0d715bcfc829ef594552c6c770bef
Chitsanupong <Chitsanupong.puegpuang@gmail.com>
2025-02-05 08:41:11 +0700

    LabSheet08_HomeWork 3, 5

---
+++ LabSheet08/src/CheckStockProduct.java
@@ -0,0 +1,73 @@
+import java.util.*;
+public class CheckStockProduct {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//ข้อ 3
+		Scanner scan = new Scanner(System.in);
+	      System.out.print("How many product list in stock: ");
+	      int productQuantity= scan.nextInt();
+	      scan.nextLine();
+	      System.out.println();
+	      Product[] productList = new Product[productQuantity];
+	 
+	 
+	      for (int i =0;i<productList.length;i++)
+	        {
+	            productList[i] = new Product();
+	            System.out.print("Input product Id : ");
+	            String id = scan.nextLine();
+	            productList[i].setId(id);
+	            System.out.print("Input product Unit : ");
+	            int unit = scan.nextInt();
+	            scan.nextLine();
+	            productList[i].setUnit(unit);          
+	            System.out.println();
+	        }
+	        //LOW//
+	        Line();
+	        System.out.println("List of product in 'LOW' status. ");  
+	        Line();    
+	        for (Product checkUnitOfProduct : productList)
+	        {
+	            if (checkUnitOfProduct.getUnit() < 5) {
+	                System.out.println(">> "+ checkUnitOfProduct.getId() + " has " + checkUnitOfProduct.getUnit() + " units");
+	            }
+	 
+	        }      
+	        //Normal//
+	        Line();
+	        System.out.println("List of product in 'NORMAL' status. ");
+	        Line();        
+	        for (Product checkUnitOfProduct : productList)
+	        {
+	            if (checkUnitOfProduct.getUnit() >= 5 && checkUnitOfProduct.getUnit() <=50) {
+	                System.out.println(">> "+ checkUnitOfProduct.getId() + " has " + checkUnitOfProduct.getUnit() + " units");
+	            }
+	 
+	        }
+	       
+	        //High//
+	        Line();
+	        System.out.println("List of product in 'HIGH' status. ");
+	        Line();      
+	        for (Product checkUnitOfProduct : productList)
+	        {
+	            if (checkUnitOfProduct.getUnit() > 50) {
+	                System.out.println(">> "+ checkUnitOfProduct.getId() + " has " + checkUnitOfProduct.getUnit() + " units");
+	            }
+	 
+	        }
+	       
+	 
+	       
+	        scan.close();
+	    }
+	    public static void Line() {
+	        System.out.println("-------------------------------------");
+	    }
+	
+
+	}
+
+
