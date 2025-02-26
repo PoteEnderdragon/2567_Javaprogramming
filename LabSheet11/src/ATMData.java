3923c1ac51ecee24842a747074352507174157be
Chitsanupong <Chitsanupong.puegpuang@gmail.com>
2025-02-26 11:19:33 +0700

    LabSheet11_Practice-4

---
+++ LabSheet11/src/ATMData.java
@@ -0,0 +1,11 @@
+
+public class ATMData {
+	private String accountNumber;
+	private String password;
+	
+	ATMData(String accountNumber,String password) {
+		this.accountNumber = accountNumber;
+		this.password = password;
+	}
+
+}
